4c07e4833fb6fdcbc85e7577ee85c88b9a7affe5
kyexmewtwo <kyexgunvolt@gmail.com>
2019-11-13 23:22:05 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-/* global $ */
-
-
-
-// DOCUMENT READY FUNCTION BELOW
-
+$("#shoot").click(function() {
+  let idea = $("#input").val();
+  $("#userChoice").html(idea);
+  let comp = Math.random();
+  if (comp > .66) {
+    var cpu = "rock";
+    $("#computerChoice").html("Rock");
+  } else if (comp > .33){
+    var cpu = "paper";
+    $("#computerChoice").html("Paper");
+  } else {
+    var cpu = "scissors";
+    $("#computerChoice").html("Scissors")
+  }
+  console.log(cpu); 
+});
