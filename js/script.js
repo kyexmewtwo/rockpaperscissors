4f2847946ae07710758552212baacd94f64702c7
kyexmewtwo <kyexgunvolt@gmail.com>
2019-11-14 04:56:00 +0000

    done

--- js/script.js
+++ js/script.js
@@ -4,13 +4,34 @@ $("#shoot").click(function() {
   let comp = Math.random();
   if (comp > .66) {
     var cpu = "rock";
-    $("#computerChoice").html("Rock");
+    $("#computerChoice").html("rock");
   } else if (comp > .33){
     var cpu = "paper";
-    $("#computerChoice").html("Paper");
+    $("#computerChoice").html("paper");
   } else {
     var cpu = "scissors";
-    $("#computerChoice").html("Scissors")
+    $("#computerChoice").html("scissors")
   }
-  console.log(cpu); 
+if (idea == 'rock' && cpu == "paper"){
+  $("#result").html("Heh. You lose.");
+} else if (idea == "rock" && cpu == "scissors"){
+  $("#result").html("Oh. You won.");
+} else if (idea == "rock" && cpu == "rock"){
+  $("#result").html("You and I think alike.");
+} else if (idea == "paper" && cpu == "scissors"){
+  $("#result").html("Heh. You lose.");
+} else if (idea == "paper" && cpu == "rock"){
+  $("#result").html("Oh. You won.");
+} else if (idea == "paper" && cpu == "paper"){
+  $("#result").html("Oh. You won.");
+} else if (idea == "scissors" && cpu == "rock"){
+  $("#result").html("Heh. You lose.");
+} else if (idea == "scissors" && cpu == "paper"){
+  $("#result").html("Oh. You won.");
+} else if (idea == "scissors" && cpu == "scissors"){
+  $("#result").html("Oh. You won.");
+}
+else{
+  $("#result").html("rock, paper, or scissors buddy, nothing else")
+  }
 });
